7afee180fcfc64c443392539c4d08fc63c4cbdd8
Volodymyr Lysenko <tibank2071@gmail.com>
2023-03-17 16:51:09 +0200

    fix:  correct style for list items

--- src/components/course/list/item/CourseListItem.tsx
+++ src/components/course/list/item/CourseListItem.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../../../hooks/hooks';
 import { fetchCourses } from '../../../../store/reducers/CoursesFetch';
+import styles from './CourseListItem.module.css';
 
 export const CourseListItem: FC<{}> = () => {
   const dispatch = useAppDispatch();
@@ -13,7 +14,12 @@ export const CourseListItem: FC<{}> = () => {
   return (
     <div>
       {courses.map((course) => (
-        <div key={course.id}>{course.title}</div>
+        <div className={styles.list} key={course.id}>
+          <div className={styles.titleWidth}>{course.title} </div>
+          <div className={styles.qtyWidth}>{course.lessonsCount}</div>
+          <div className={styles.skillsWidth}></div>
+          <div className={styles.ratingWidth}>{course.rating}</div>
+        </div>
       ))}
     </div>
   );
