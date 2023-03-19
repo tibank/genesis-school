5094e81372e3ff60fb77adeb5f7bcc0d93631821
Volodymyr Lysenko <tibank2071@gmail.com>
2023-03-19 11:01:37 +0200

    fix: fix some error css style

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
+import { ErrorMessage } from './components/common/ErrorMessage';
 import { CoursesListHeader } from './components/course/list/header/CoursesListHeader';
 import { CourseListItem } from './components/course/list/item/CourseListItem';
 
 function App() {
-  return (
-    <div className='App'>
-      <CoursesListHeader />
-      <CourseListItem />
-    </div>
-  );
+    return (
+        <div className='App'>
+            <CoursesListHeader />
+            <CourseListItem />
+        </div>
+    );
 }
 
 export default App;
